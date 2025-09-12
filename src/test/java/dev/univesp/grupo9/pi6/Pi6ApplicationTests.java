cd16a088e8bf394ea448fe18d5069dd7363a6937
Klayton Facre <klayton.facre@hotmail.com>
2025-09-12 16:11:01 -0300

    test: desabilita auto-config de DB/Flyway em Pi6ApplicationTests

--- src/test/java/dev/univesp/grupo9/pi6/Pi6ApplicationTests.java
+++ src/test/java/dev/univesp/grupo9/pi6/Pi6ApplicationTests.java
@@ -1,9 +1,18 @@
 package dev.univesp.grupo9.pi6;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        FlywayAutoConfiguration.class
+})
 class Pi6ApplicationTests {
 
 	@Test
